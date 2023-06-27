3d2a02f444f79d93de8d60f516008dd6a4dff725
Adrian Hodos <adi.hodos@gmail.com>
2023-06-27 22:59:45 +0300

    Updated emacs

--- emacs/vanilla/.emacs.d/init.el
+++ emacs/vanilla/.emacs.d/init.el
@@ -1,73 +1,240 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(setq inhibit-startup-message t)
 
-(straight-use-package 'use-package)
+(require 'package)
+(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(setq package-user-dir (expand-file-name "elpa/" user-emacs-directory))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package no-littering :ensure t)
+
+(recentf-mode 1)
+(setq recentf-max-saved-items 100
+      inhibit-startup-message t
+      ring-bell-function 'ignore)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(global-hl-line-mode 1)
+(global-display-line-numbers-mode 1)
+
+(use-package gruber-darker-theme :ensure)
+(load-theme 'gruber-darker t)
+
+(use-package command-log-mode :ensure t)
+(use-package all-the-icons :ensure t)
+
+(use-package rainbow-delimiters :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode)
+  ;; :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3)
+  (setq which-key-popup-type 'side-window)
+
+  ;; location of which-key window. valid values: top, bottom, left, right,
+  ;; or a list of any of the two. If it's a list, which-key will always try
+  ;; the first location first. It will go to the second location if there is
+  ;; not enough room to display any keys in the first location
+  (setq which-key-side-window-location 'bottom)
+
+  ;; max width of which-key window, when displayed at left or right.
+  ;; valid values: number of columns (integer), or percentage out of current
+  ;; frame's width (float larger than 0 and smaller than 1)
+  (setq which-key-side-window-max-width 0.33)
+
+  ;; max height of which-key window, when displayed at top or bottom.
+  ;; valid values: number of lines (integer), or percentage out of current
+  ;; frame's height (float larger than 0 and smaller than 1)
+  (setq which-key-side-window-max-height 0.25)
+  
+  )
+
+(set-face-attribute 'default nil :font "Iosevka SS03" :weight 'normal :height 150)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka SS03" :weight 'light :height 150)
+(set-face-attribute 'variable-pitch nil :font "Iosevka SS03" :weight 'light :height 150)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; (when (file-directory-p "~/Projects/Code")
+  ;;   (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :ensure t
+ :after projectile
+ :config
+ (counsel-projectile-mode 1))
 
 (use-package rustic
-  :straight (
-	     :config (
-		      (setq rustic-format-on-save nil
-			    rustic-lsp-format t
-			    rustic-lsp-server 'rust-analyzer
-;;			    lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer")
-			    lsp-rust-analyzer-server-display-inlay-hints t
-			    lsp-rust-analyzer-display-chaining-hints t
-			    lsp-rust-analyzer-display-parameter-hints t
-			    rustic-format-display-method 'ignore
-			    rustic-indent-method-chain t
-			    rustic-format-on-save nil
-			    rustic-lsp-format t
-			    rustic-format-trigger 'on-save))))
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  ;; (setq rustic-format-on-save t)
+  ;; (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
+  )
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  ;; (add-hook 'before-save-hook 'lsp-format-buffer nil t)
+  )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ba4ab079778624e2eadbdc5d9345e6ada531dc3febeb24d257e6d31d5ed02577" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" default))
+ '(package-selected-packages
+   '(doom-themes selectrum flycheck yasnippet company lsp-ui lsp-mode multiple-cursors winum glsl-mode no-littering all-the-icons-dired toml-mode rustic use-package))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (use-package lsp-mode
-  :straight (
-	     :hook (
-		    (rustic-mode . lsp))
-	     :config (
-		      (setq lsp-prefer-flymake nil))
-	     
-	     :commands lsp))
-(use-package lsp-ui :straight (
-			       :config(
-				       (setq lsp-ui-doc-enable t
-					     lsp-ui-doc-use-webkit t
-					     lsp-ui-doc-position 'top
-					     lsp-ui-doc-include-signature t
-					     lsp-ui-doc-header t
-					     lsp-ui-sideline-enable nil
-					     lsp-ui-flycheck-enable t
-					     lsp-ui-flycheck-list-position 'right
-					     lsp-ui-flycheck-live-reporting t
-					     lsp-ui-peek-enable t
-					     lsp-ui-peek-list-width 60
-					     lsp-ui-peek-peek-height 25)
-				       )
-			       :hook (lsp-mode . lsp-ui)
-			       :commands lsp-ui-mode))
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  ;; (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints nil)
+  ;; (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints nil)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-doc-enable t))
 
 (use-package company
-  :straight (
-	     :config (
-		      (setq company-idle-delay 0.3)
-		      (global-company-mode 1)
-		      (global-set-key (kbd "C-<tab>") 'company-complete))))
+  :ensure
+  :custom
+  (company-idle-delay 0.5) ;; how long to wait until popup
+  ;; (company-begin-commands nil) ;; uncomment to disable popup
+  :bind
+  (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last)))
 
-(use-package company-lsp
-  :straight (
-	     :config (
-		      (push 'company-lsp company-backends)
-		      (setq company-transformers nil
-			    company-lsp-async t
-			    company-lsp-cache-candidates nil))))
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
 
-(setq display-line-numbers-type t)
+(use-package flycheck :ensure)
+(use-package toml-mode :ensure)
+
+(use-package selectrum
+  :ensure
+  :init
+  (selectrum-mode)
+  :custom
+  (completion-styles '(flex substring partial-completion)))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :config)
+
+(use-package all-the-icons-dired :ensure
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;;(load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package glsl-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode)))
+
+;; ;; Some common sense settings
+
+;; start every frame maximized
+(add-to-list 'default-frame-alist '(fullscreen . fullboth))
+
+(use-package winum :ensure t)
+(winum-mode)
+
+(use-package ibuffer :ensure t
+  :config (global-set-key (kbd "C-x C-b") 'ibuffer)
+  )
+
+(use-package multiple-cursors :ensure t
+  :config (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
+(setq backup-directory-alist            '((".*" . "~/.Trash")))
