c2df499307996dc274a93c28424f226003f28f56
Adi Hodos <adi.hodos@gmail.com>
2024-11-11 11:03:41 +0200

    Updated Nix flake

--- .dotfiles/nixos/configuration.nix
+++ .dotfiles/nixos/configuration.nix
@@ -81,6 +81,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    logiops
+    piper
+
     wget
     p7zip
     unzip
@@ -192,5 +195,9 @@
       IdleAction=hibernate
       IdleActionSec=90m
     '';
+  };
+
+  services.ratbagd = {
+    enable = true;
   };
 }
